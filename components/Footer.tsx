1835ee7212941d41f719bd6fccdc219d180f09dc
David Polanía Mejía <david.polania3@gmail.com>
2025-09-14 04:38:20 -0500

    feature: clean and ghpages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,18 +8,8 @@ type FooterProps = {
 export default function Footer({ about, social }: FooterProps) {
   return (
     <footer className={styles.footer}>
-      <section className={styles.about}>
-        <p className={styles.aboutText}>{about}</p>
-      </section>
-
-      <section className={styles.social}>
-        <ul>
-          {social.map((item, index) => (
-            <li key={index}>
-              <a href={item.href}>{item.label}</a>
-            </li>
-          ))}
-        </ul>
+      <section className={styles.logo}>
+        <img src="/images/dpmsnotes_logo.jpg" alt="logo" />
       </section>
     </footer>
   );
