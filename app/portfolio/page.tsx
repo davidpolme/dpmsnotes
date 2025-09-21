de38f1cff7759df4a3b6dfc240deddd6b9af2ff2
David Polanía Mejía <david.polania3@gmail.com>
2025-09-21 00:09:49 -0500

    feature: add back button to portfolio and blog pages. Additionall convert footer logo into home button

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,7 @@
 import Items from "@/components/Items";
 import { getPortfolioItems } from "@/lib/portfolio";
 import styles from "@/styles/PortfolioPage.module.scss";
+import Link from "next/link";
 
 export default function PortfolioPage() {
   const items = getPortfolioItems();
@@ -8,6 +9,11 @@ export default function PortfolioPage() {
   return (
     <main className={styles.portfolioPage}>
       <h1>Portfolio</h1>
+
+      <Link href="/" className={styles.backButton}>
+        ← Volver al inicio
+      </Link>
+
       <div className={styles.itemsWrapper}>
         <Items data={items} />
       </div>
