add266fe0f056879bda3443c2b79c14e3e3e16e5
davidpolme <davidpolme@users.noreply.github.com>
2025-09-28 07:45:19 +0000

    deploy: 1b41b960f786abceaac1d11309ecc6dcee4c5423

---
+++ _next/static/chunks/16914c462eda278d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/55e611984230868a.js",
+  "static/chunks/turbopack-049ccfeac5da4ad1.js"
+])
---
+++ _next/static/yPtfTja8A7dM5Ixqob7SK/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/687477c92999f3f9.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dpmsnotes//_next/:path+",
+        "destination": "/dpmsnotes/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
